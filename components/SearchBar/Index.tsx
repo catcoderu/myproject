26be0b25ec94543cf4e30dfd66b7cc1bb3f300f1
catcoderu <ruhana199904@gmail.com>
2024-01-30 11:53:17 +0400

    added buttons

--- components/SearchBar/Index.tsx
+++ components/SearchBar/Index.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { CiSearch } from "react-icons/ci";
 import styles from "./style.module.css"
+import { IoPersonOutline } from "react-icons/io5";
+import { FiShoppingCart } from "react-icons/fi";
 
 const SearchBar = () => {
   return (
     <form >
       <div className={styles.search}>
         <input className={styles.searchbar} type="search" placeholder='Bura yazın' />
-        <button className={styles.btn}><CiSearch /></button></div>
+        <button className={styles.btn}><CiSearch /></button>
+        <button className={styles.personbtn}><IoPersonOutline /></button>
+        <button className={styles.shopbtn}><FiShoppingCart /></button>
+        </div>
+        
     </form>
+    
   );
 }
 
